b7033f19c452ae54dc2af5b4c50bc3e8edaba71b
metehn <metehanersoy@gmail.com>
2022-08-09 11:33:40 +0300

    @Produce

---
+++ src/main/java/com/metehan/cdi/_02_Produce.java
@@ -0,0 +1,32 @@
+package com.metehan.cdi;
+
+
+import lombok.*;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Builder
+@Data
+@Named(value = "producedtuto")
+@ApplicationScoped
+public class _02_Produce {
+
+    @Produces
+    public List<String> getList(){
+        List<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("Android");
+        list.add("Kotlin");
+        list.add("Spring");
+
+        return list;
+    }
+
+}
